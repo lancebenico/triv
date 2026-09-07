71735bc1818d57337635949cbecd3fe5a841947f
Lance Aidrian Benico <lanceaidrian.benico.cics@ust.edu.ph>
2026-09-07 23:36:19 +0800

    Add high-resolution TRIV icon favicons in multiple sizes

--- public/career.php
+++ public/career.php
@@ -31,7 +31,10 @@ $departments = array_unique(array_column($allJobs, 'department'));
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Careers - TRIV Design & Construction</title>
-    <link rel="icon" type="image/png" href="../assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/apple-touch-icon.png">
+    <link rel="shortcut icon" href="../assets/images/favicon.png">
     <link rel="stylesheet" href="../assets/css/public-style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
